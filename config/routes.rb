c1af7e722397439092b0a6a7acd91b92dacd2334
melaniejae <mtaurus575@gmail.com>
2015-04-04 19:24:26 +0000

    Created quotes controller, html integrated .erb file for front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Splurty::Application.routes.draw do
+Splurty::Application.routes.draw do 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +53,6 @@ Splurty::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'quotes#index'
 end
