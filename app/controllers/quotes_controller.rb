e9d4450b44d29593ca2d71c974aa079791f379e3
melaniejae <mtaurus575@gmail.com>
2015-04-05 16:52:30 +0000

    first attempt at adding quotes from the rails console command line into the database

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,7 @@
 class QuotesController < ApplicationController
 	def index
+		@quote = quote.first
+# @quote is variable, first quote in database Quote
+# is now the string-type value of @aquote		
 	end	
 end
