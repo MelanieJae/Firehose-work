e212853b7c8bf48057fafceef0946ff321dba1e6
MelanieJae <mtaurus575@gmail.com>
2015-05-03 02:32:24 +0000

    added unique tag to model and index view and tested it

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -18,4 +18,12 @@ class Quote < ActiveRecord::Base
 # 		puts @first_initial.concat(@last_initial)
 #	end
 
+  	def unique_tag
+  		abbr = self.author.split(" ").collect do |sub_string|
+  			sub_string[0]
+    	end
+
+    	abbr.join + '#' + self.id.to_s
+    end
+
 end
